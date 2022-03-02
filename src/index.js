aba95b7fa069cf3d4b279c7f4465cee95b909895
Kevawn Warren <kevawnw@gmail.com>
2022-03-02 11:14:04 -0500

    commit tree f9ce92f9073b81dca74cfd119932755dcc224ae6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
+  const inputForm = document.querySelector('form')
+  inputForm.addEventListener('submit', (event) => {event.preventDefault();
+    const input = document.querySelector('input#searchByID')
+    // the value of the input data by user
+    console.log(input.value)
+
+    //console.log(event.target.children[1].value) - This one helps to find the location of where the data would go.
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+    const title = document.querySelector('section#movieDetails h4');
+    const summary = document.querySelector('section#movieDetails p');
+
+    title.innerText = data.title;
+    summary.innerText = data.summary
+
+    })
+  })
+ 
   
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
